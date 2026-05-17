1988da66febbd65eabc93c26db24a16ce9a4f36f
BrillerMo <14666070+brillermo@user.noreply.gitee.com>
2026-05-17 14:52:21 +0800

    引入Sentinel Dashboard

--- src/main/java/com/whu/medicalbackend/MedicalBackendApplication.java
+++ src/main/java/com/whu/medicalbackend/MedicalBackendApplication.java
@@ -3,12 +3,14 @@ package com.whu.medicalbackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableAspectJAutoProxy
 @MapperScan({
         "com.whu.medicalbackend.medical.mapper",
         "com.whu.medicalbackend.family.mapper",
---
+++ src/main/java/com/whu/medicalbackend/common/config/SentinelWebConfig.java
@@ -0,0 +1,28 @@
+package com.whu.medicalbackend.common.config;
+
+import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
+import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class SentinelWebConfig {
+
+    @PostConstruct
+    public void init() {
+        WebCallbackManager.setUrlBlockHandler(new SentinelBlockHandler());
+    }
+
+    @Bean
+    public FilterRegistrationBean<CommonFilter> sentinelFilterRegistration() {
+        FilterRegistrationBean<CommonFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CommonFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("sentinelFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+}
